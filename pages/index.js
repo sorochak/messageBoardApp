981b5a9f8fbf45c5408985187b880f4cd64c4232
AustenSorochak <austensorochak@gmail.com>
2022-02-02 21:24:12 -0800

    added Components from Lab 1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import MessageItem from '../components/MessageItem';
 import MessageList from '../components/MessageList';
+import MessageItem from '../components/MessageItem';
 
 export default function App() {
   return (
