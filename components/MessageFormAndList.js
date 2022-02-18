0d6f9fa9f631d1a3b0392a9c304ba7a21ea0c500
AustenSorochak <austensorochak@gmail.com>
2022-02-18 12:34:47 -0800

    updated MessageFormAndList.js and index.js to pass jsonData as props to fetch data from the api

--- components/MessageFormAndList.js
+++ components/MessageFormAndList.js
@@ -2,42 +2,42 @@ import NewMessageForm from "./NewMessageForm";
 import MessageList from "./MessageList";
 import { useState } from 'react';
 
-const MessageFormAndList = () => {
+const MessageFormAndList = ({ jsonData }) => {
 
-    const initialState = [
-        {
-            id: 0,
-            name: "Bill",
-            msgText: "I don't know. Philosophize with him!",
-        },
-        {
-            id: 1,
-            name: "Ted",
-            msgText: "All we are is dust in the wind, dude.",
-        },
-        {
-            id: 2,
-            name: "Soldier",
-            msgText: "Are you suggesting coconuts migrate?",
-        },
-        {
-            id: 3,
-            name: "The Black Knight",
-            msgText: "Just a flesh wound.",
-        },
-        {
-            id: 4,
-            name: "God",
-            msgText: "Course it's a good idea!",
-        },
-        {
-            id: 5,
-            name: "Hunter S. Thompson",
-            msgText: "Too weird to live, too rare to die!",
-        },
-    ];
+    // const initialState = [
+    //     {
+    //         id: 0,
+    //         name: "Bill",
+    //         msgText: "I don't know. Philosophize with him!",
+    //     },
+    //     {
+    //         id: 1,
+    //         name: "Ted",
+    //         msgText: "All we are is dust in the wind, dude.",
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "Soldier",
+    //         msgText: "Are you suggesting coconuts migrate?",
+    //     },
+    //     {
+    //         id: 3,
+    //         name: "The Black Knight",
+    //         msgText: "Just a flesh wound.",
+    //     },
+    //     {
+    //         id: 4,
+    //         name: "God",
+    //         msgText: "Course it's a good idea!",
+    //     },
+    //     {
+    //         id: 5,
+    //         name: "Hunter S. Thompson",
+    //         msgText: "Too weird to live, too rare to die!",
+    //     },
+    // ];
 
-    const [messages, setMessages] = useState(initialState);
+    const [messages, setMessages] = useState(jsonData);
 
 
     // function called by Formik to pass data from the Form
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,26 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MessageFormAndList from '../components/MessageFormAndList';
 import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
-export default function App() {
+export async function getStaticProps() {
+    let jsonData;
+
+    try {
+        const { data } = await axios.get('http://localhost:3004/api/messages');
+        jsonData = data;
+    } catch(error) {
+        console.log('API Error: ' + error);
+    }
+
+    return {
+        props: {
+            jsonData
+        }
+    }
+}
+
+export default function App( { jsonData }) {
   return (
                 <Container>
                     <Row className="d-flex justify-content-center">
@@ -13,7 +31,7 @@ export default function App() {
                     </Row>
                     <Row className="d-flex justify-content-center">
                         <Col lg={8}>
-                            <MessageFormAndList />
+                            <MessageFormAndList jsonData = {jsonData} />
                         </Col>  
                     </Row>
                     <Row className="d-flex justify-content-center">
